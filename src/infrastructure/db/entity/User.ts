ec1ef174b2d287c37906b5df894dfd0b91dd1efb
yude <i@yude.jp>
2022-12-08 10:35:35 +0000

    feat: User テーブルの構成を更新

--- src/infrastructure/db/entity/User.ts
+++ src/infrastructure/db/entity/User.ts
@@ -1,5 +1,10 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
+export enum UserRole {
+    STAFF = "staff",
+    MEMBER = "member",
+}
+
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
@@ -10,4 +15,11 @@ export class User {
 
     @Column()
     password!: string;
+
+    @Column({
+        type: "enum",
+        enum: UserRole,
+        default: UserRole.MEMBER,
+    })
+    role!: UserRole;
 }
