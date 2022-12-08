666a4c9a19e67e23265d839521fd2cd9c14d166a
yude <i@yude.jp>
2022-12-08 11:05:12 +0000

    feat: add dummy datum

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,11 @@ import "reflect-metadata"
 
 import { AppDataSource } from "./infrastructure/db/data-source";
 import { News } from "./infrastructure/db/entity/News";
+import { Inquiry } from "./infrastructure/db/entity/Inquiry";
+import { Disaster } from "./infrastructure/db/entity/Disaster";
 
 AppDataSource.initialize()
-    .then(() => {
+    .then(async () => {
 
         const app = express()
 
@@ -29,14 +31,60 @@ AppDataSource.initialize()
 
         // TODO: delete this
         // Add fake entry for debugging
-        const news = new News()
-        news.author_id = 0
-        news.title = "Lorem ipsum"
-        news.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci sem, dictum id augue quis, pharetra mollis metus. Morbi rutrum id tellus vitae egestas. Nullam hendrerit, urna a cursus finibus, mi turpis hendrerit est, maximus egestas magna dolor fermentum massa. Duis sagittis bibendum neque, scelerisque ele"
-        news.tag = ["test"]
-        news.datetime = new Date()
-        news.isPublished = true
+        const res = await AppDataSource.getRepository(News)
+            .createQueryBuilder('entity')
+            .getOne();
+        if (res === null) {
+            for (let i = 0; i < 10; i++) {
+                const news = new News()
+                news.author_id = 0
+                news.title = "Lorem ipsum"
+                news.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci sem, dictum id augue quis, pharetra mollis metus. Morbi rutrum id tellus vitae egestas. Nullam hendrerit, urna a cursus finibus, mi turpis hendrerit est, maximus egestas magna dolor fermentum massa. Duis sagittis bibendum neque, scelerisque ele"
+                news.tag = ["test"]
+                news.datetime = new Date()
+                news.isPublished = true
 
-        AppDataSource.manager.save(news)
+                await AppDataSource.manager.save(news)
+            }
+        }
+
+        const res2 = await AppDataSource.getRepository(Inquiry)
+            .createQueryBuilder('entity')
+            .getOne()
+
+        if (res2 === undefined) {
+            for (let i = 0; i < 10; i++) {
+                const inquiry = new Inquiry()
+                inquiry.user_id = 0
+                inquiry.title = "Lorem ipsum"
+                inquiry.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci sem, dictum id augue quis, pharetra mollis metus. Morbi rutrum id tellus vitae egestas. Nullam hendrerit, urna a cursus finibus, mi turpis hendrerit est, maximus egestas magna dolor fermentum massa. Duis sagittis bibendum neque, scelerisque ele"
+                inquiry.datetime = new Date()
+                inquiry.isPublished = true
+
+                AppDataSource.manager.save(inquiry)
+            }
+        }
+
+        const res3 = await AppDataSource.getRepository(Disaster)
+            .createQueryBuilder('entity')
+            .getOne();
+
+        if (res3 === null) {
+            console.log("aaa")
+            for (let i = 0; i < 10; i++) {
+                const disaster = new Disaster()
+                disaster.author_id = 0
+                disaster.title = "Lorem ipsum"
+                disaster.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci sem, dictum id augue quis, pharetra mollis metus. Morbi rutrum id tellus vitae egestas. Nullam hendrerit, urna a cursus finibus, mi turpis hendrerit est, maximus egestas magna dolor fermentum massa. Duis sagittis bibendum neque, scelerisque ele"
+                disaster.datetime = new Date()
+                disaster.tag = ["test"]
+                disaster.isPublished = true
+
+                AppDataSource.manager.save(disaster)
+            }
+        }
+
+        // TODO: delete this ここまで
+
     })
     .catch((error) => console.log(error))
