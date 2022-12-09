6020b80556cc5e1dc1ff54876825fca8124bc605
yude <i@yude.jp>
2022-12-09 10:07:39 +0000

    feat: add inquiry

---
+++ src/infrastructure/db/utils.ts
@@ -0,0 +1,13 @@
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+
+async function getUserNameById(user_id: number) {
+    console.log(user_id)
+    const user_name = await AppDataSource.getRepository(User)
+        .createQueryBuilder("user")
+        .where("user.user_id = :user_id", { user_id: user_id })
+        .getOne()
+    return user_name
+}
+
+export { getUserNameById }
--- src/routes/admin/inquiry.ts
+++ src/routes/admin/inquiry.ts
@@ -1,13 +1,26 @@
 import express from "express";
 import { AppDataSource } from "../../infrastructure/db/data-source";
 import { Inquiry } from "../../infrastructure/db/entity/Inquiry";
+import moment, { Moment } from "moment";
+import "moment-timezone"
+import { getUserNameById } from "../../infrastructure/db/utils";
 const router = express.Router()
 
 router.get('/', async (req: express.Request, res: express.Response) => {
+    moment.tz.setDefault('Asia/Tokyo');
+
     const inquiry = await AppDataSource.getRepository(Inquiry)
         .createQueryBuilder('entity')
         .getMany();
-    res.render('pages/admin/inquiry', { inquiry: inquiry })
+
+    let user_name = []
+    for (let i = 0; i < inquiry.length; i++) {
+        user_name.push(await getUserNameById(inquiry[i].user_id))
+    }
+
+    console.log(user_name)
+
+    res.render('pages/admin/inquiry', { inquiry: inquiry, moment: moment, user_name: user_name })
 })
 
 export default router
