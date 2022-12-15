6b84fc371b076f07ce81be5dd3d7b3e611de76ed
Okazaki Kohei <marie673g@gmail.com>
2022-12-15 12:04:01 +0900

    [clean]インライン化

--- src/infrastructure/db/utils.ts
+++ src/infrastructure/db/utils.ts
@@ -1,12 +1,11 @@
-import { AppDataSource } from "./data-source";
-import { User } from "./entity/User";
+import {AppDataSource} from "./data-source";
+import {User} from "./entity/User";
 
 async function getUserById(user_id: number) {
-    const user = await AppDataSource.getRepository(User)
+    return await AppDataSource.getRepository(User)
         .createQueryBuilder("user")
-        .where("user.user_id = :user_id", { user_id: user_id })
+        .where("user.user_id = :user_id", {user_id: user_id})
         .getOne()
-    return user
 }
 
 export { getUserById }
