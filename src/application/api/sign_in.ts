58e7812b6800f51528ab81e53caa56ff15760d7e
Okazaki Kohei <marie673g@gmail.com>
2023-01-12 06:53:01 +0900

    [add] サインインの実装

--- src/application/api/sign_in.ts
+++ src/application/api/sign_in.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import * as UserRepository from "../../domain/repository/User"
+import {User, UserRole} from "../../domain/entity/User";
+
 
 const router = express.Router()
 
 router.post('/', async (req: express.Request, res: express.Response) => {
 
-    // TODO
-    // dbにユーザーが存在するか問い合わせ
-    // いなければdbに登録して成功
+    if (await UserRepository.find(req.body.uuid) != null) {
+        res.json({ success: false, message: 'Duplicate user_id.' })
+    }
+
+    const user: User = {
+        user_id: Number(req.body.uuid),
+        name: req.body.name,
+        password: req.body.password,
+        role: UserRole.MEMBER,
+        fmc_token: req.body.fmc_token
+    }
+    await UserRepository.save(user)
+    res.json({ success: true, message: "Successful account creation"})
 })
 
 export default router
--- src/domain/repository/User.ts
+++ src/domain/repository/User.ts
@@ -26,4 +26,3 @@ export async function remove(user_id: number): Promise<boolean> {
 
     return false
 }
-
