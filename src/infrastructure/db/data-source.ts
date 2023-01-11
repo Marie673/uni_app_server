5d4942d67a815058bf538b8a21dd4fc7a0a8ff28
Okazaki Kohei <okazaki@net.info.hiroshima-cu.ac.jp>
2023-01-11 19:13:37 +0900

    Update data-source.ts

--- src/infrastructure/db/data-source.ts
+++ src/infrastructure/db/data-source.ts
@@ -8,11 +8,11 @@ import {Timetable} from "../../domain/entity/Timetable";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "uni-app-database.cknwwzyfwuur.ap-northeast-1.rds.amazonaws.com",
     port: 3306,
-    username: "user",
-    password: "password",
-    database: "testdb",
+    username: "admin",
+    password: "qRIl24zoLx2e00Qp6Lsf",
+    database: "test",
     synchronize: true,
     logging: false,
     entities: [User, News, Inquiry, Disaster, Timetable],
