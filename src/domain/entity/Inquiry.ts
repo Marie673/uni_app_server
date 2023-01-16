eb27d1284205ed280c92a72478a1721929e33997
Kohei Okazaki <okazaki@net.info.hiroshima-cu.ac.jp>
2023-01-16 14:39:07 +0900

    [add] Inquiry機能の実装

--- src/domain/entity/Inquiry.ts
+++ src/domain/entity/Inquiry.ts
@@ -3,20 +3,29 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity()
 export class Inquiry {
     @PrimaryGeneratedColumn()
-    readonly inquiry_id!: number;
+    readonly inquiry_id?: number;
 
     @Column()
-    user_id!: number;
+    user_id: number;
 
     @Column("text")
-    title!: string;
+    title: string;
 
     @Column("text")
-    content!: string;
+    content: string;
 
     @Column()
-    datetime!: Date;
+    datetime: Date;
 
     @Column()
-    isPublished!: boolean;
+    isPublished: boolean;
+
+    constructor(user_id: number, title: string, content: string,
+                datetime: Date, isPublished: boolean) {
+        this.user_id = user_id
+        this.title = title
+        this.content = content
+        this.datetime = datetime
+        this.isPublished = isPublished
+    }
 }
